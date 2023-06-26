c5e0b87f1b5a9bfbc2ac65ff801cfb10419d6d4f
Ekremunalytu <ekremunalw1@gmail.com>
2023-06-26 21:54:14 +0300

    grade calculation added

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ struct points {
     int visa_1;
     int visa_2;
     int the_final;
+    double grade;
+    string result;
 };
 
 struct subjects {
@@ -22,6 +24,8 @@ struct students {
 };
 
 void print_data(students student[] , string Lessons[4] , int student_count);
+void grade_calculator(students student[] ,  int student_count);
+
 
 int main() {
 
@@ -35,6 +39,7 @@ int main() {
     students student[10];
 
     string Lessons[4] = {"Calculus" , "Structural Programming" , "Circuit Theory" , "Semiconductor Physics"};
+    string note_gap[9] = {"AA" , "BA" , "BB" , "CB" , "CC" , "DC" , "DD" , "FD" , "FF"};
 
    
     for (int i = 0 ; i < student_count ; i++){
@@ -48,7 +53,7 @@ int main() {
         
         for (int j = 0 ; j < 4 ; j++){
             
-            cout << "Enter " << Lessons[j] << "  exam  points\n" << endl;
+            cout << "Enter " << Lessons[j] << "  Exam  points\n" << endl;
 
             cout << "Fist visa:  ";
             cin >> student[i].lessons[j].student_points.visa_1;
@@ -64,34 +69,39 @@ int main() {
         
 
     }
-
+    grade_calculator(student , student_count);
     print_data(student , Lessons , student_count);
 
     return 0;
 }
 
 void print_data(students student[], string Lessons[4], int student_count) {
-    int col_width = 12;
+    int col_width = 8;
 
     cout << "Printing student data" << endl;
     cout << left << setw(col_width) << "Name"
          << setw(col_width) << "Number";
+              
 
     for (int j = 0; j < 4; j++) {
+        
         cout << " | "
-             << setw(col_width * 3) << Lessons[j];
+             << setw(col_width * 3) << Lessons[j] << setw(col_width*2);
+
     }
     cout << endl;
-
     cout << setw(col_width) << ""
          << setw(col_width) << "";
 
     for (int j = 0; j < 4; j++) {
+
         cout << " | "
              << setw(col_width) << "Visa 1"
              << setw(col_width) << "Visa 2"
-             << setw(col_width) << "Final";
+             << setw(col_width) << "Final"
+             << setw(col_width) << "grade";
     }
+
     cout << endl;
 
     for (int i = 0; i < student_count; i++) {
@@ -102,8 +112,34 @@ void print_data(students student[], string Lessons[4], int student_count) {
             cout << " | "
                  << setw(col_width) << student[i].lessons[j].student_points.visa_1
                  << setw(col_width) << student[i].lessons[j].student_points.visa_2
-                 << setw(col_width) << student[i].lessons[j].student_points.the_final;
+                 << setw(col_width) << student[i].lessons[j].student_points.the_final
+                 << setw(col_width) << student[i].lessons[j].student_points.grade;
         }
         cout << endl;
     }
-// dont share sensitive information
+
+}
+
+void grade_calculator(students student[] ,  int student_count){
+
+    double sum = 0;
+
+    for (int i = 0 ; i < student_count ; i++){
+
+        for (int j = 0 ; j < 4 ; j++){
+            sum  +=  (student[i].lessons[j].student_points.visa_1 * 0.3);
+            sum  +=  (student[i].lessons[j].student_points.visa_2 * 0.3);
+            sum  +=  (student[i].lessons[j].student_points.the_final * 0.4);
+
+            student[i].lessons[j].student_points.grade = sum;
+
+            sum = 0;
+        }
+
+    }
+
+
+
+
+
+}
